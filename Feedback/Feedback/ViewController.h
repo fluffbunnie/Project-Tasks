bdde8cd0cdb67a6a3436dd73f8b5bb1a4bb92149
minhthao <minh commit>
2015-04-15 17:29:31 -0700

    add feedback

---
+++ Feedback/Feedback/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Feedback
+//
+//  Created by minh thao nguyen on 4/15/15.
+//  Copyright (c) 2015 Magpie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Feedback/Feedback/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Feedback
+//
+//  Created by minh thao nguyen on 4/15/15.
+//  Copyright (c) 2015 Magpie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#define FEEDBACK_TARGETED_EMAIL "feedback@getmagpie.com"
+
+@interface ViewController ()
+@property (nonatomic, strong) NSString *userObjectId; //this is just a string to identify who send the feedback message
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
